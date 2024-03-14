c03c3220cf5fac32fac76a72ee142a1743ecab0b
seitamuro <seitamuro0210@gmail.com>
2024-03-14 16:01:43 +0900

    feat(client): CSSを使ってhero画像サイズを調整するようにした

--- src/client/components/foundation/AspectRatio/AspectRatio.tsx
+++ src/client/components/foundation/AspectRatio/AspectRatio.tsx
@@ -1,8 +1,4 @@
 import type { FC, ReactNode } from 'react';
-import { useEffect, useRef, useState } from 'react';
-import { throttle } from 'throttle-debounce';
-
-import * as styles from './AspectRatio.styles';
 
 type Props = {
   ratioWidth: number;
@@ -11,30 +7,8 @@ type Props = {
 };
 
 export const AspectRatio: FC<Props> = ({ children, ratioHeight, ratioWidth }) => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const [clientHeight, setClientHeight] = useState<number>(0);
-
-  useEffect(() => {
-    const updateClientHeight = throttle(1000, () => {
-      const width = containerRef.current?.getBoundingClientRect().width ?? 0;
-      const height = (width * ratioHeight) / ratioWidth;
-      setClientHeight(height);
-    });
-
-    let timer = (function tick() {
-      return setImmediate(() => {
-        updateClientHeight();
-        timer = tick();
-      });
-    })();
-
-    return () => {
-      clearImmediate(timer);
-    };
-  }, [ratioHeight, ratioWidth]);
-
   return (
-    <div ref={containerRef} className={styles.container({ clientHeight })}>
+    <div style={{ aspectRatio: `${ratioWidth} / ${ratioHeight}`, height: "100%", width: "100%" }}>
       {children}
     </div>
   );
--- src/client/components/product/ProductHeroImage/ProductHeroImage.tsx
+++ src/client/components/product/ProductHeroImage/ProductHeroImage.tsx
@@ -1,13 +1,12 @@
 import classNames from 'classnames';
 import { isEqual } from "lodash-es";
-import { memo, useEffect, useState } from 'react';
+import { memo } from 'react';
 import type { FC } from 'react';
 
 import type { ProductFragmentResponse } from '../../../graphql/fragments';
 import { Anchor } from '../../foundation/Anchor';
 import { AspectRatio } from '../../foundation/AspectRatio';
 import { DeviceType, GetDeviceType } from '../../foundation/GetDeviceType';
-import { WidthRestriction } from '../../foundation/WidthRestriction';
 
 import * as styles from './ProductHeroImage.styles';
 
@@ -21,34 +20,36 @@ export const ProductHeroImage: FC<Props> = memo(({ product, title }) => {
     <GetDeviceType>
       {({ deviceType }) => {
         return (
-          <WidthRestriction>
-            <Anchor href={`/product/${product?.id}`}>
-              <div className={styles.container()}>
-                <AspectRatio ratioHeight={9} ratioWidth={16}>
-                  <img className={styles.image()} src={product?.media[0].file.filename} />
-                </AspectRatio>
+          <div style={{ width: "100%" }}>
+            <div style={{ margin: "0 auto", maxWidth: "1024px", width: "100%" }}>
+              <Anchor href={`/product/${product?.id}`}>
+                <div className={styles.container()}>
+                  <AspectRatio ratioHeight={9} ratioWidth={16}>
+                    <img className={styles.image()} src={product?.media[0].file.filename} />
+                  </AspectRatio>
 
-                <div className={styles.overlay()}>
-                  <p
-                    className={classNames(styles.title(), {
-                      [styles.title__desktop()]: deviceType === DeviceType.DESKTOP,
-                      [styles.title__mobile()]: deviceType === DeviceType.MOBILE,
-                    })}
-                  >
-                    {title}
-                  </p>
-                  <p
-                    className={classNames(styles.description(), {
-                      [styles.description__desktop()]: deviceType === DeviceType.DESKTOP,
-                      [styles.description__mobile()]: deviceType === DeviceType.MOBILE,
-                    })}
-                  >
-                    {product?.name}
-                  </p>
+                  <div className={styles.overlay()}>
+                    <p
+                      className={classNames(styles.title(), {
+                        [styles.title__desktop()]: deviceType === DeviceType.DESKTOP,
+                        [styles.title__mobile()]: deviceType === DeviceType.MOBILE,
+                      })}
+                    >
+                      {title}
+                    </p>
+                    <p
+                      className={classNames(styles.description(), {
+                        [styles.description__desktop()]: deviceType === DeviceType.DESKTOP,
+                        [styles.description__mobile()]: deviceType === DeviceType.MOBILE,
+                      })}
+                    >
+                      {product?.name}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </Anchor>
-          </WidthRestriction>
+              </Anchor>
+            </div>
+          </div>
         );
       }}
     </GetDeviceType>
