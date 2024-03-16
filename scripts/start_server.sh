c34c45a8aac27839b6ff6c4bde22c213b3bd23ac
seitamuro <seitamuro0210@gmail.com>
2024-03-16 14:10:24 +0900

    fix(cicd): サーバー停止と起動の方法を変更

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cd ../
 ./bin/node_modules/.bin/ts-node ./src/server/index.ts
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 pid = ps aux | grep ts-node | grep -v grep | awk '{print $2}'
+if [ ! -z "$pid" ]; then
+  kill -9 $pid
+else
