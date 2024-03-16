43ffdac2418362babfe47471f8b62fa9a66ef476
seitamuro <seitamuro0210@gmail.com>
2024-03-16 14:27:38 +0900

    fix(cicd): サーバー停止のスクリプトのプロセスIDの取得方法を修正

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pid = ps aux | grep ts-node | grep -v grep | awk '{print $2}'
+pid=$(ps aux | grep ts-node | grep -v grep | awk '{print $2}')
 if [ ! -z "$pid" ]; then
   kill -9 $pid
 fi
