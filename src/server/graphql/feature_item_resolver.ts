af30743a3ae4b9feb8ed393f8f28a65d0fc48415
seitamuro <seitamuro0210@gmail.com>
2024-03-16 16:11:13 +0900

    feat(server): GraphQLサーバーのN+1問題を解決

--- src/server/graphql/feature_item_resolver.ts
+++ src/server/graphql/feature_item_resolver.ts
@@ -1,9 +1,21 @@
+import DataLoader from 'dataloader';
+
 import type { FeatureItem } from '../../model/feature_item';
+import { Product } from '../../model/product';
+import { dataSource } from '../data_source';
 
 import type { GraphQLModelResolver } from './model_resolver';
 
+const ProductLoader = new DataLoader(async (ids: readonly number[]) => {
+  const products = await dataSource
+    .createQueryBuilder(Product, 'product')
+    .whereInIds(ids)
+    .select(['product.id', 'product.name', 'product.price', 'product.description'])
+    .getMany();
+
+  return ids.map((id) => products.find((product) => product.id === id)) as Product[];
+});
+
 export const featureItemResolver: GraphQLModelResolver<FeatureItem> = {
-  product: (parent) => {
-    return parent.product;
-  },
+  product: async (parent) => await ProductLoader.load(parent.id),
 };
